8e8ef2e5aa5f6762008010393c6dd842527306d7
David Shakesby <david.shakesby@cef.co.uk>
2024-11-28 23:51:27 +0000

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header :class="theme">
+    <div class="fl ai-c jc-sb">
+      <h1>Personal Project</h1>
+      <!-- Toggle Switch -->
+      <div class="fl ai-c">
+        <text class="mr-10">Theme</text>
+        <label class="switch">
+          <input type="checkbox" @change="toggleTheme" :checked="theme === 'dark'" />
+          <span class="slider"></span>
+        </label>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    // Map state to component
+    ...mapState(['theme'])
+  },
+  methods: {
+    // Map mutation to method
+    ...mapMutations(['toggleTheme'])
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    padding: 1em;
+    text-align: center;
+  }
+</style>
